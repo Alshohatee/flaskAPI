52a1e3f9b1b7f86e5ee6f60384cc43acda724f2c
alshohatee <alshohateeaseel@gmail.com>
2021-10-23 22:42:36 -0400

    adding the plants route

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -33,7 +33,16 @@ def create_app(test_config=None):
             'plants':formatted_plants[start:end],
             'total_plants': len(formatted_plants)
         })
-
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+        if plant is None:
+            abort(404)
+        else:   
+            return jsonify({
+                'success': True,
+                'plant': plant.format()
+            })
 
     @app.route('/entrees/<int:entree_id>')
     def retrieve_entree(entree_id):
